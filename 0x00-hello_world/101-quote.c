c7fe501f0cab776d152e1e12daa4b3494d92dd1e
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-05 16:41:14 -0700

    C programm that print wwithout printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * main - UNIX is basically a simple operating system.
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
