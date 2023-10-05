f51cf6f41b26b9853e2993a7947c3389c6c1d40e
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-05 14:25:32 -0700

    C programm print using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program print using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammae, but the outcome is a piece of art,\n");
+	return (0);
+}
+
