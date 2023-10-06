a63344aa806baf53ec10ff538fa0ac56e8f24bf3
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-06 09:21:51 -0700

    Task 4 Ex README

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i < 132; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(i);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
