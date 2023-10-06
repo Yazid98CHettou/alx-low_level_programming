29bd654a7fc53ae0cd45b51976a24441349a058c
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-06 09:18:27 -0700

    program that prints the alphabet in lowercase, Task 4 EX in README

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - When I was having that alphabet soup,
+ * I never thought that it would pay off
+ * Return: ALWAYS 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 132; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
