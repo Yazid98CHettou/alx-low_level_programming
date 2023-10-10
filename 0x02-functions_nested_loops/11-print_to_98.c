325566378e9db9ed64037a7a79b5fdd0726d8880
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-10 14:22:49 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,9 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-			{
 				printf("%d,", i);
-			}
 			else if (i == 98)
-			{
 				printf("%d\n", i);
-			}
 		}
 	}
 	else if (n >= 98)
@@ -29,13 +25,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-			{
 				printf("%d,", j);
-			}
 			else if (j == 98)
-			{
 				printf("%d\n", j);
-			}
 		}
 	}
 }
