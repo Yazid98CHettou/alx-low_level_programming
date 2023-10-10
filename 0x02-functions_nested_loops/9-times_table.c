e1a2c85a7243a363019261b1cb140874b470306f
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-10 12:30:32 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,14 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
-				_putchar('');
-				_putchar('');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
 				_putchar(',');
-				_putchar('');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
