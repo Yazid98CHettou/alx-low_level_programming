f2339ddf25194378f5f2471d3e8ea8083741c1d0
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-12 09:14:54 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: cher to check
+ * Return: 0 or 1
+ */
+int_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
