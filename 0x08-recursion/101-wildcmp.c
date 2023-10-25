1b6584d567e7e135e95f718c956449a06e6e3ba9
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-25 10:55:07 -0700

    8. Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,8 +3,9 @@
  * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
- * Return : 0
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
