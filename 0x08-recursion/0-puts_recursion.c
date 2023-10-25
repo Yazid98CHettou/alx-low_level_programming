3895bbea377a43b933e0cb00298edfeb37aebc23
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-25 08:03:44 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return : Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursions(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
