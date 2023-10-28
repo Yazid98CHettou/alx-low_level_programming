ff66fcd162e0ad62a851f1a84c8df9352bb89fa2
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-10-27 17:35:29 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0(Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _strlen - returns the lengh of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
+
