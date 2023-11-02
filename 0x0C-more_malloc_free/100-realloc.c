79a190d97221ae6d708cf84bd4961ff7214c1e7e
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-11-02 07:45:00 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  * if newçsize == old_size, returns ptr without changes
  * if malloc fails, returns NULL
  */
-void *_realloc(void *ptr, undigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
 	{
