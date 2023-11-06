364fc0494280d9637407737bfdd8839996601072
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-11-06 09:13:44 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,23 +1,26 @@
-#include"dog.h"
-#include<stdio.h>
+#include "dog.h"
+#include <stdio.h>
+
 /**
- * print_dog - print a struct dog
- * @d:struct dog
- * Return : no retrun
+ * print_dog - prints a struct dog.
+ * @d: struct dog.
+ *
+ * Return: no return.
  */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
 		if (!(d->name))
-			printf("Nme: (nil)\n");
+			printf("Name: (nil)\n");
 		else
-			printf("Name: %s`\n", d->name);
+			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
-		if(!(d->owner))
-			printf("owner: (nil)\n");
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
 		else
-			printf("owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
-
