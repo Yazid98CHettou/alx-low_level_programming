8537f1cb671e2fe4cfff8a798258628be228fc9f
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-11-08 04:50:23 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,16 @@
-#include"3-calc.h"
+#include "3-calc.h"
+
 /**
- * get_op_func - select the correct fucntion to perform
- * the operation asked by the user
- * @s: char operator
- * Return: pointer to the function that correspond to the operator
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"*", op_add},
+		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
@@ -23,6 +25,6 @@ int (*get_op_func(char *s))(int, int)
 			break;
 		i++;
 	}
+
 	return (ops[i / 2].f);
 }
-
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,11 @@
-#include"3-calc.h"
+#include "3-calc.h"
+
 /**
- * main - check the code for Holberton school students
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -15,20 +17,24 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	operation = get_op_func(argv[2]);
+
 	if (operation == NULL)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
 	printf("%d\n", operation(a, b));
 	return (0);
 }
-
