5927e05305671c37b732f07cab9b97b58913d7a7
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2023-11-08 03:31:11 -0800

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include"function_pointers.h"
 /**
- * array_iterator - executes a function given as a 
+ * array_iterator - executes a function given as a
  * parameters on each element of an array
  * @array: input integer array
  * @size: size of the array
